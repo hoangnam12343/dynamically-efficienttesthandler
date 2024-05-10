67ade4eaffa0117b019ecfc4a06770a18c4fcaf8
hoangnam12343 <guilhermo3pun0suy@hotmail.com>
2024-05-10 19:15:58 +0700

    update sdk task

--- index.js
+++ index.js
@@ -1,15 +1,2 @@
-function subsetsWithDup(nums) {
-  const result = [];
-  nums.sort((a, b) => a - b);
-  backtrack(0, []);
-  return result;
-  function backtrack(start, current) {
-    result.push([...current]);
-    for (let i = start; i < nums.length; i++) {
-      if (i > start && nums[i] === nums[i - 1]) continue;
-      current.push(nums[i]);
-      backtrack(i + 1, current);
-      current.pop();
-    }
-  }
-}
+const nthElement = (arr, n = 0) =>
+  (n > 0 ? arr.slice(n, n + 1) : arr.slice(n))[0];
